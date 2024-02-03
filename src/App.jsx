9fa9d66eb9666ab1d5b89b42fbf1b974e53a3c58
ElGunner79 <richard.brazao@outlook.com>
2024-02-03 14:47:43 +0800

    Added stub pages, home and users

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./layouts";
+import Home from "./pages/Home";
+import Users from "./pages/Users";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ path: "/", element: <div>About</div> }],
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/users", element: <Users />}
+    ],
   },
 ]);
 
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
+
+// This is the home page of our website. It simply displays a heading saying Home"
